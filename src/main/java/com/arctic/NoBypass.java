cdf90a2a9ec6281535bc0c73a705dd8539018d84
Larrox <153541835+copyandbuild@users.noreply.github.com>
2025-03-18 18:23:32 +0100

    Fixed Bugs & Typos - corrected typo in config.yml - added Larrox as a author - added prefix - improved word checks

--- src/main/java/com/arctic/NoBypass.java
+++ src/main/java/com/arctic/NoBypass.java
@@ -4,51 +4,76 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class NoBypass extends JavaPlugin implements Listener {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoBypass extends JavaPlugin implements Listener, TabCompleter {
+    private List<String> swearWords;
+    private boolean isMsgTrue;
+    private String responses;
+    private String responsecommand;
+    private boolean isTrue;
+    private String prefix = "§8[§aNoBypass§8] §7";
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("nobypass").setExecutor(this::reloadCommand);
         saveDefaultConfig();
-        getLogger().info(ChatColor.GREEN + "NoBypass has been enabled");
+        loadConfigValues();
     }
 
+    private void loadConfigValues() {
         FileConfiguration config = getConfig();
-        List<String> swearWords = config.getStringList("words");
-        Boolean isMsgTrue = config.getBoolean("messageEnable", false);
-        String responses = config.getString("message");
-        Boolean isTrue = config.getBoolean("commandEnable", false);
-
+        swearWords = config.getStringList("words").stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+        isMsgTrue = config.getBoolean("messageEnable", false);
+        responses = config.getString("message", "");
+        responsecommand = config.getString("command-message", "");
+        isTrue = config.getBoolean("commandEnable", false);
+    }
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String message = event.getMessage().toLowerCase();
 
-        String filteredMessage = message.replaceAll("[^a-z]", "");;
+        String[] words = message.split("\\s+");
 
-        String[] words = filteredMessage.split("\\s+");
-
-        for (int i = 0; i < words.length; i++) {
-            if (swearWords.contains(words[i].toLowerCase())) {
+        for (String word : words) {
+            if (swearWords.contains(word)) {
                 event.setCancelled(true);
-                if (isMsgTrue.equals(true)) {
+                if (isMsgTrue) {
                     player.sendMessage(ChatColor.RED + responses);
                 }
-                if (isTrue.equals(true)) {
+                if (isTrue) {
                     punishPlayer(player);
                 }
+                return;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        String command = event.getMessage().toLowerCase();
+
+        for (String word : swearWords) {
+            if (command.contains(word)) {
+                event.setCancelled(true);
+                
+                player.sendMessage(responsecommand);
                 return;
             }
         }
@@ -57,30 +82,21 @@ public class NoBypass extends JavaPlugin implements Listener {
     public boolean reloadCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
             reloadConfig();
-            FileConfiguration config = getConfig();
-            swearWords = config.getStringList("words");
-            isMsgTrue = config.getBoolean("messageEnable", false);
-            responses = config.getString("message");
-            isTrue = config.getBoolean("commandEnable", false);
-
-            sender.sendMessage(ChatColor.GREEN + "NoBypass config.yml successfully reloaded.");
+            loadConfigValues();
+            sender.sendMessage(prefix + "config §asuccessfully§7 reloaded.");
             return true;
         } else {
-            sender.sendMessage(ChatColor.RED + "Usage: /nobypass reload");
+            sender.sendMessage(prefix + "Usage: §c/nobypass reload");
             return false;
         }
     }
 
-
     private void punishPlayer(Player player) {
         FileConfiguration config = getConfig();
-        String Command = config.getString("command");
-            if (Command != null) {
-                String command = Command.replace("%player%", player.getName());
-                getServer().getScheduler().runTask(this, () -> {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
-                });
-            }
+        String command = config.getString("command");
+        if (command != null) {
+            String executedCommand = command.replace("%player%", player.getName());
+            getServer().getScheduler().runTask(this, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), executedCommand));
+        }
     }
-
 }
